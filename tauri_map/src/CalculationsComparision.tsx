1e5eaebd5246f092ed251abfcd437b4d7d66be08
Tim Dalton <daltontf1212@gmail.com>
2026-06-26 15:31:16 -0500

    Missed files

---
+++ tauri_map/src/CalculationsComparision.tsx
@@ -0,0 +1,11 @@
+import { type LeagueStats } from "./Structs";
+
+interface CalculationsComparisionProps {
+    priorCalculations: LeagueStats | null;
+    calculations: LeagueStats | null;
+}
+
+
+export default function CalculationsComparision({priorCalculations, calculations}: CalculationsComparisionProps) {
+    return <></>
+} 
---
+++ tauri_map/src/Structs.tsx
@@ -0,0 +1,43 @@
+export interface Team {
+    name: string;
+    L: number;
+    S: number;
+    N: number;
+    color: string;
+    state: string | [string];
+    coordinates: {
+        lat: number;
+        lon: number;
+    }
+}
+
+export interface League {
+    league_name: string,
+    weight: number,
+    teams: Team[],
+}
+
+export interface TeamStats {
+    team_name: string,
+    share: number,
+    share_population: number,
+    share_population_value: number,
+    color: string | undefined
+}
+
+export interface CountyStats {
+    county_name: string,
+    state_name: string,
+    population: number,
+    team_stats: [TeamStats],
+}
+
+export interface LeagueStats {
+    league_name: string,
+    county_stats_by_geoid: Map<number, CountyStats>,
+}
+
+export interface CalculatorInteface {
+  getCalculationsForLeague: (league:any) => Promise<LeagueStats>,
+  getStateForCoordinates: (lat:number, lon:number) => Promise<string>
+}
